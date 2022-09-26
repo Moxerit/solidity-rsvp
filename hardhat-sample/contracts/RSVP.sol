1f7fc301c2efb69be88e505ba00285095ae8d0df
Chase Alber <calber207@gmail.com>
2022-09-26 13:49:27 -0600

    removed initialized git repo in folder

---
+++ hardhat-sample/contracts/RSVP.sol
@@ -0,0 +1,179 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract RSVP {
+    event NewEventCreated(
+        bytes32 eventID,
+        address creatorAddress,
+        uint256 eventTimestamp,
+        uint256 maxCapacity,
+        uint256 deposit,
+        string eventDataCID
+    );
+
+    event NewRSVP(bytes32 eventID, address attendeeAddress);
+
+    event ConfirmedAttendee(bytes32 eventID, address attendeeAddress);
+
+    event DepositsPaidOut(bytes32 eventID);
+    struct CreateEvent {
+        bytes32 eventId;
+        string eventDataCID;
+        address eventOwner;
+        uint256 eventTimestamp;
+        uint256 deposit;
+        uint256 maxCapacity;
+        address[] confirmedRSVPs;
+        address[] claimedRSVPs;
+        bool paidOut;
+    }
+
+    mapping(bytes32 => CreateEvent) public idToEvent;
+
+    function createNewEvent(
+        uint256 eventTimestamp,
+        uint256 deposit,
+        uint256 maxCapacity,
+        string calldata eventDataCID
+    ) external {
+        bytes32 eventId = keccak256(
+            abi.encodePacked(
+                msg.sender,
+                address(this),
+                eventTimestamp,
+                deposit,
+                maxCapacity
+            )
+        );
+
+        require(
+            idToEvent[eventId].eventTimestamp == 0,
+            "This event has already been registered."
+        );
+
+        address[] memory confirmedRSVPs;
+        address[] memory claimedRSVPs;
+
+        idToEvent[eventId] = CreateEvent(
+            eventId,
+            eventDataCID,
+            msg.sender,
+            eventTimestamp,
+            deposit,
+            maxCapacity,
+            confirmedRSVPs,
+            claimedRSVPs,
+            false
+        );
+
+        emit NewEventCreated(
+            eventId,
+            msg.sender,
+            eventTimestamp,
+            maxCapacity,
+            deposit,
+            eventDataCID
+        );
+    }
+
+    function createNewRSVP(bytes32 eventId) external payable {
+        CreateEvent storage myEvent = idToEvent[eventId];
+
+        require(
+            msg.value == myEvent.deposit,
+            "You must pay the deposit to RSVP."
+        );
+
+        require(
+            block.timestamp <= myEvent.eventTimestamp,
+            "You cannot RSVP after the event has started."
+        );
+
+        require(
+            myEvent.confirmedRSVPs.length < myEvent.maxCapacity,
+            "This event is at max capacity."
+        );
+
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            require(
+                myEvent.confirmedRSVPs[i] != msg.sender,
+                "You have already RSVP'd to this event."
+            );
+        }
+
+        myEvent.confirmedRSVPs.push(payable(msg.sender));
+        emit NewRSVP(eventId, msg.sender);
+    }
+
+    function confirmAttendee(bytes32 eventId, address attendee) public {
+        CreateEvent storage myEvent = idToEvent[eventId];
+
+        require(msg.sender == myEvent.eventOwner, "Not owner.");
+
+        address rsvpConfirm;
+
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            if (myEvent.confirmedRSVPs[i] == attendee) {
+                rsvpConfirm = myEvent.confirmedRSVPs[i];
+            }
+        }
+
+        require(rsvpConfirm == attendee, "No reservations found.");
+
+        for (uint8 i = 0; i < myEvent.claimedRSVPs.length; i++) {
+            require(myEvent.claimedRSVPs[i] != attendee, "Already checked in.");
+        }
+
+        require(myEvent.paidOut == false, "Refund already claimed.");
+
+        myEvent.claimedRSVPs.push(attendee);
+
+        (bool sent, ) = attendee.call{value: myEvent.deposit}("");
+
+        if (!sent) {
+            myEvent.claimedRSVPs.pop();
+        }
+
+        require(sent, "Failed to send Ether.");
+        emit ConfirmedAttendee(eventId, attendee);
+    }
+
+    function confirmAllAttendees(bytes32 eventId) external {
+        CreateEvent memory myEvent = idToEvent[eventId];
+
+        require(msg.sender == myEvent.eventOwner, "Not owner.");
+
+        for (uint8 i = 0; i < myEvent.confirmedRSVPs.length; i++) {
+            confirmAttendee(eventId, myEvent.confirmedRSVPs[i]);
+        }
+    }
+
+    function withdrawUnclaimedDeposits(bytes32 eventId) external {
+        CreateEvent memory myEvent = idToEvent[eventId];
+
+        require(!myEvent.paidOut, "Already claimed and paid out.");
+
+        require(
+            block.timestamp >= (myEvent.eventTimestamp + 7 days),
+            "Cannot check in more than 7 days away."
+        );
+
+        require(msg.sender == myEvent.eventOwner, "Only owner.");
+
+        uint256 unclaimed = myEvent.confirmedRSVPs.length -
+            myEvent.claimedRSVPs.length;
+
+        uint256 payout = unclaimed * myEvent.deposit;
+
+        myEvent.paidOut = true;
+
+        (bool sent, ) = msg.sender.call{value: payout}("");
+
+        if (!sent) {
+            myEvent.paidOut = false;
+        }
+
+        require(sent, "Failed to send Ether");
+        emit DepositsPaidOut(eventId);
+    }
+}
